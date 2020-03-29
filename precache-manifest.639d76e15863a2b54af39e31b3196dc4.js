63d62d152bbec40146026dd407c1d817cb710281
Afshawn Lotfi <me@afshawn.org>
2020-03-29 19:43:24 -0400

    updates

---
+++ precache-manifest.639d76e15863a2b54af39e31b3196dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46800b341f4a919d6a84ae4bdef8a789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4cd810ee5d3f205db32",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "36a4ca54e12bfa2fb935",
+    "url": "/static/js/2.37d77555.chunk.js"
+  },
+  {
+    "revision": "917eaf209788165d81ce0cc951066387",
+    "url": "/static/js/2.37d77555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cd810ee5d3f205db32",
+    "url": "/static/js/main.f33f7a2f.chunk.js"
+  },
+  {
+    "revision": "8212fd330cd0cea0feec",
+    "url": "/static/js/runtime-main.2f05f7c1.js"
+  }
+]);
